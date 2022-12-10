5054b75ea3237905c21ad06de196f6bbfa382801
Dinyanya <ismagambetova161@gmail.com>
2022-12-10 17:42:50 +0600

    Desgined UpdateFragment Layout

--- app/src/main/java/com/example/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/todoapp/fragments/list/ListFragment.kt
@@ -5,8 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.RecyclerView
 import com.example.todoapp.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
@@ -23,6 +24,10 @@ class ListFragment : Fragment() {
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
         }
 
+        val list = view.findViewById<ConstraintLayout>(R.id.listLayout)
+        list.setOnClickListener{
+            findNavController().navigate(R.id.action_listFragment_to_updateFragment)
+        }
         return view
     }
 
