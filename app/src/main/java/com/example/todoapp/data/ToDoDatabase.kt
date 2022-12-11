edc1f92d9b760d4a829dd60dc6afe77bd3af2a89
Dinyanya <ismagambetova161@gmail.com>
2022-12-11 12:53:07 +0600

    Create TypeConverter, Repository & ViewModel

--- app/src/main/java/com/example/todoapp/data/ToDoDatabase.kt
+++ app/src/main/java/com/example/todoapp/data/ToDoDatabase.kt
@@ -4,10 +4,14 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.todoapp.data.models.ToDoData
 import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.internal.synchronized
 
 @Database(entities = [ToDoData::class], version = 1, exportSchema = false)
+@TypeConverters(Converter::class)
+
 abstract class ToDoDatabase: RoomDatabase() {
 
     abstract fun todoDataAccessObjects(): ToDoDataAccessObjects
---
+++ app/src/main/java/com/example/todoapp/data/repository/ToDoRepository.kt
@@ -0,0 +1,14 @@
+package com.example.todoapp.data.repository
+
+import androidx.lifecycle.LiveData
+import com.example.todoapp.data.ToDoDataAccessObjects
+import com.example.todoapp.data.models.ToDoData
+
+class ToDoRepository(private val toDoDataAccessObjects: ToDoDataAccessObjects) {
+
+    val getAllData: LiveData<List<ToDoData>> = toDoDataAccessObjects.getAllData()
+
+    suspend fun insertData(toDoData: ToDoData) {
+        toDoDataAccessObjects.insertData(toDoData)
+    }
+}
